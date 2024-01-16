de14fe8b8474d9c02b67963ae647e29308a66a0e
akhtar-muntazir123 <91075831+akhtar-muntazir123@users.noreply.github.com>
2024-01-16 22:10:52 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a="harry";
+const b="muntazir";
+const c="areeb";
+const d="sahil";
+
+export default c;
+export {a};
+export {b};
+export {d};
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import ui,{a,b,d} from "./module1.mjs"
+//if we change a to a1 it will show an error
+console.log(ui);
+console.log(a);
+console.log(b);
+console.log(d);
